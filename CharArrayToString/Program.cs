676dd17851c5e8e2db3aa3e64d7350fcae79cd35
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-26 23:51:11 +0200

    Add files via upload

---
+++ CharArrayToString/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CharArrayToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = { 'B', 'a', 'r', 't', 'e', 'k' };
+            string s1 = new string(array);
+            Console.WriteLine(s1);
+        }
+    }
+}
